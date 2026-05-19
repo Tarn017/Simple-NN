803bd28460395f5922f55d4083733822b8440a67
Tobias <to-bi.arnold@web.de>
2026-05-19 14:45:07 +0200

    Format and clean up setup_linux.sh script

--- setup_linux.sh
+++ setup_linux.sh
@@ -2,12 +2,12 @@ ls
 echo "[INFO] entered .venv/"
 pip install --upgrade pip
 echo "[INFO] upgraded pip"
-pip install -r requirements_class.txt
+pip install -r requirements_class.txt 
 echo "[INFO] non-torch libraries installed"
 mkdir -p tmp_pip
 echo "[INFO] tmp_pip cache-folder created"
 export TMPDIR=$(pwd)/tmp_pip
-pip install --cache-dir="$TMPDIR" -r requirements_torch.txt
+pip install --cache-dir="$TMPDIR" -r requirements_torch.txt 
 echo "[INFO] torch libraries installed"
 rm -r tmp_pip
 echo "[INFO] deleted tmp cache"
