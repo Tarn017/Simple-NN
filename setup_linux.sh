acf20a2263bc0249a27d1214b3fac50fb0a68b84
Tobias <to-bi.arnold@web.de>
2026-05-19 13:31:03 +0200

    Add files via upload

---
+++ setup_linux.sh
@@ -0,0 +1,15 @@
+ls
+echo "[INFO] entered .venv/"
+python3 -m ensurepip --upgrade
+echo "[INFO] upgraded pip"
+pip install -r requirements_class.txt
+echo "[INFO] non-torch libraries installed"
+mkdir -p tmp_pip
+echo "[INFO] tmp_pip cache-folder created"
+export TMPDIR=$(pwd)/tmp_pip
+pip install --cache-dir="$TMPDIR" -r requirements_torch.txt
+echo "[INFO] torch libraries installed"
+rm -r tmp_pip
+echo "[INFO] deleted tmp cache"
+echo "------------------------------------------------------------"
+echo "fertig! :)"
